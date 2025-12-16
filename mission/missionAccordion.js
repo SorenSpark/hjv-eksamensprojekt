5eba7bb438948dc6bc82e7fd1486aef044be08f3
Annemette M <annemettemygind@gmail.com>
2025-12-16 20:47:00 +0100

    nye moduler - ryddet op i kode

--- mission/missionAccordion.js
+++ mission/missionAccordion.js
@@ -1,31 +1,3 @@
-// function enableAccordion(header, wrapper, card, startOpen = false) {
-//   const chevron = header.querySelector(".accordion-icon");
-
-//   // Starttilstand
-//   if (startOpen) {
-//     wrapper.classList.remove("collapsed");
-//     card.classList.add("is-open");
-//     card.classList.remove("is-collapsed");
-//     chevron.classList.add("rotated");
-//   } else {
-//     wrapper.classList.add("collapsed");
-//     card.classList.add("is-collapsed");
-//     card.classList.remove("is-open");
-//     chevron.classList.remove("rotated");
-//   }
-
-//   // Klik på header
-//   header.onclick = () => {
-//     const isCollapsed = wrapper.classList.contains("collapsed");
-
-//     wrapper.classList.toggle("collapsed");
-//     card.classList.toggle("is-open", isCollapsed);
-//     card.classList.toggle("is-collapsed", !isCollapsed);
-
-//     chevron.classList.toggle("rotated", isCollapsed);
-//   };
-// }
-
 export function enableAccordion(header, wrapper, card, startOpen = false) {
   const chevron = header.querySelector(".accordion-icon");
 

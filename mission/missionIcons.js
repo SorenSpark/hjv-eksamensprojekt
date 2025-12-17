9d26b950f02e7398f6cb06ff1a0d44da4a2d26d0
Annemette M <annemettemygind@gmail.com>
2025-12-17 18:29:59 +0100

    test af ikon type funktion

--- mission/missionIcons.js
+++ mission/missionIcons.js
@@ -1,12 +1,14 @@
 //Her tjekker vi hvilket environment missionen er i, og sætter passende ikon ind
 
-export function getTaskTypeIcon(env) {
+function getTaskTypeIcon(env) {
   switch (env) {
     case "Land":
       return "terrain";
     case "Vand":
       return "water";
     default:
-      return "help"; // fallback
+      return "help";
   }
 }
+
+module.exports = { getTaskTypeIcon };
